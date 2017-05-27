7212b6b86ec5101b019943df23b7dd9aa7d1bbb4
senselogic <ecstatic.coder@gmail.com>
2017-05-27 10:46:34 +0200

    % test

--- TEST/run.sh
+++ TEST/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -x
-../resync --changed --removed --added --exclude ".git/" --exclude "*/.git/" --exclude "*.tmp" --print --confirm --preview SOURCE_FOLDER/ TARGET_FOLDER/
-../resync --changed --removed --added --print --confirm --create --preview SOURCE_FOLDER/ CREATED_FOLDER/
-../resync --changed --removed --added --print --confirm --create --preview SOURCE_FOLDER/ TARGET_FOLDER/
-../resync --changed --removed --added --verbose --preview --compare always SOURCE_FOLDER/ TARGET_FOLDER/
+../resync --updated --changed --removed --added --exclude ".git/" --exclude "*/.git/" --exclude "*.tmp" --print --confirm --preview SOURCE_FOLDER/ TARGET_FOLDER/
+../resync --updated --changed --removed --added --print --confirm --create --preview SOURCE_FOLDER/ CREATED_FOLDER/
+../resync --updated --changed --removed --added --print --confirm --create --preview SOURCE_FOLDER/ TARGET_FOLDER/
+../resync --updated --changed --removed --added --verbose --preview --compare always SOURCE_FOLDER/ TARGET_FOLDER/
 ../resync --updated --removed --added --preview SOURCE_FOLDER/ TARGET_FOLDER/
 ../resync --moved --preview SOURCE_FOLDER/ TARGET_FOLDER/
